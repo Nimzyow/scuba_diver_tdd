8164cbedcb96c1d336f124c9a426b8837a854cf6
Nimzyow <n_soufiani@hotmail.com>
2020-02-09 16:38:55 +0000

    seperated second pressure group test to different file

--- spec/pressure_group_spec.rb
+++ spec/pressure_group_spec.rb
@@ -279,24 +279,3 @@ RSpec.describe Pressure_group do
     end
   end
   end
-
-  RSpec.describe Pressure_group do
-    def second_pressure_group(initial_pressure_group:, minutes:)
-      subject.determine_second_pressure_group({initial_pressure_group: initial_pressure_group, minutes: minutes})
-    end
-
-    it "has determine second pressure group method accepting two arguments" do
-      expect(subject).to respond_to(:determine_second_pressure_group).with_keywords(:initial_pressure_group, :minutes), "determine_second_pressure_group method needs to pass two keyword arguments"
-    end
-
-    context "initial pressure group is L" do
-      it "return H when surface interval is 24 minutes" do
-        expect(second_pressure_group(initial_pressure_group: "L", minutes: 24)).to eq("H") 
-      end
-    end
-    context "initial pressure group is P" do
-      it "return G when surface interval is 46" do
-        expect(second_pressure_group(initial_pressure_group:"P", minutes: 46)).to eq("G")
-      end
-    end
-  end
